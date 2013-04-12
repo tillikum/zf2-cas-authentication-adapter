9f87fb33a76f77d49ff1bec4e85aae82613513d8
Gordon Stratton <gordon.stratton@gmail.com>
2013-04-12 18:33:59 +0000

    Formatting fixes

--- src/Tillikum/Authentication/CasResult.php
+++ src/Tillikum/Authentication/CasResult.php
@@ -1,38 +1,39 @@
 <?php
+
 namespace Tillikum\Authentication;
 
 use Zend\Authentication\Result;
 
 /**
- * Class CasResult
- * Extension of Zend's Authentication\Result class which contains additional information about the Authentication
- * Result.
+ * Contains additional information about CAS authentication results
  */
 class CasResult extends Result
 {
-
     /**
      * The contents of the result of the CAS Message
+     *
      * @var string
      */
     protected $resultBody;
 
     /**
      * Sets the result code, identity, and failure messages
-     * @param int $code
-     * @param mixed $identity
-     * @param array $messages
+     *
+     * @param int         $code
+     * @param mixed       $identity
+     * @param array       $messages
      * @param string|null $resultBody
      */
     public function __construct($code, $identity, array $messages = array(), $resultBody = null)
     {
         parent::__construct($code, $identity, $messages);
+
         $this->resultBody = $resultBody;
     }
 
-
     /**
      * Sets the body of the result message returned from the CAS Server.
+     *
      * @param string $resultBody
      */
     public function setResultBody($resultBody)
@@ -42,6 +43,7 @@ class CasResult extends Result
 
     /**
      * Gets the body of the result message returned from the CAS Server.
+     *
      * @return string|null
      */
     public function getResultBody()
